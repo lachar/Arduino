1b968e805642f15a849a934f66ccd30610780f05
Earle F. Philhower, III <earlephilhower@yahoo.com>
2020-07-09 14:24:54 -0700

    Revert "GitHub CI testing"

--- package/deploy_package_index.sh
+++ package/deploy_package_index.sh
@@ -10,14 +10,14 @@ cd $(dirname "$0")
 # Decrypt and install SSH private key.
 # "encrypted_xxx_key" and "encrypted_xxx_iv" are environment variables
 # known to Travis CI builds.
-openssl enc -d -aes-256-cbc -K $GHKEY -iv $GHIV -in esp8266_github_io_deploy.enc -out esp8266_github_io_deploy
+openssl aes-256-cbc -K $encrypted_3a94a4db7dec_key -iv $encrypted_3a94a4db7dec_iv -in esp8266_github_io_deploy.enc -out esp8266_github_io_deploy -d
 eval "$(ssh-agent -s)"
 chmod 600 esp8266_github_io_deploy
 ssh-add esp8266_github_io_deploy
 echo -e "Host github.com\nStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 # Clone the Github pages repository
-git clone git@github.com:earlephilhower/esp8266.github.io.git
+git clone git@github.com:esp8266/esp8266.github.io.git
 pushd esp8266.github.io
 
 # Update the package index
@@ -25,8 +25,8 @@ cp ../versions/$tag/package_esp8266com_index.json stable/package_esp8266com_inde
 git add stable/package_esp8266com_index.json
 
 # Commit and push the changes
-git config user.email "github-ci-action@github.com"
-git config user.name "GitHub CI Action"
-git commit -m "Update package index for release $tag"
+git config user.email "travis@travis-ci.org"
+git config user.name "Travis CI"
+git commit -m "update package index for release $tag"
 git push origin master
 popd
